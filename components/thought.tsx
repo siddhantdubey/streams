d4a447350263b0614ec6ee35750a43de8a515539
siddhantdubey <sid12.dubey@gmail.com>
2023-05-08 13:51:51 -0400

    initial setup

---
+++ components/thought.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+interface ThoughtProps {
+    thought: {
+        createdAt: string;
+        content: string;
+    };
+}
+
+const Thought: React.FC<ThoughtProps> = ({ thought }) => {
+    const date = new Date(thought.createdAt);
+
+    const formattedDate = `${date.getFullYear()}/${String(date.getMonth() + 1).padStart(2, '0')}/${String(date.getDate()).padStart(2, '0')} ${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
+    return (
+        <div className="flex w-full justify-center">
+            <div className="w-1/2">
+                <div className="flex flex-row justify-start items-center mb-4">
+                    <div className="text-gray-500 text-lg">{formattedDate}</div>
+                    <div className="text-black text-lg ml-4">{thought.content}</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Thought;
